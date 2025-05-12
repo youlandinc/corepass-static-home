d169162a8507203f897bc5c867f4f87f4aca8126
lee <eric@youland.com>
2025-05-12 19:05:28 +0800

    update

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -13,7 +13,7 @@ export function NavLink({
   return (
     <div
       className="inline-block cursor-pointer rounded-lg px-2 py-1 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
-      onClick={() => router.push(href)}
+      onClick={() => location.href = href}
     >
       {children}
     </div>
